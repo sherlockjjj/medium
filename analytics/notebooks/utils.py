48a8a83bdafc0ae854fd676debf47dcd86f8b581
Kevin Liao <lwk723@berkeley.edu>
2018-09-05 14:03:35 -0700

    add utils

--- analytics/notebooks/utils.py
+++ analytics/notebooks/utils.py
@@ -1,7 +1,5 @@
+import nltk
 import pandas as pd
-import spacy
-
-nlp = spacy.load('en')
 
 
 def parse_date(x):
@@ -17,10 +15,64 @@ def parse_date(x):
     )
 
 
-def word_count(x):
+def tokenizer(text):
     """
-    count the number of words
+    convert a string to list of string
     """
-    return pd.DataFrame(
-        {'{}_wc'.format(x.name): x.apply(lambda text: len(text.split(' ')))})
+    return nltk.tokenize.word_tokenize(text)
 
+
+def get_stopwords(tokens):
+    """
+    get stopword tokens
+    """
+    stopwords = nltk.corpus.stopwords.words('english')
+    return [t for t in tokens if t.lower() in stopwords]
+
+
+def filter_stopwords(tokens):
+    """
+    drop stopword tokens
+    """
+    stopwords = nltk.corpus.stopwords.words('english')
+    return [t for t in tokens if t.lower() not in stopwords]
+
+
+def get_unusual_words(tokens):
+    """
+    get unusual word tokens
+    """
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    return [t for t in tokens if t.lower() not in english_vocab]
+
+
+def filter_unusual_words(tokens):
+    """
+    drop unusual word tokens
+    """
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    return [t for t in tokens if t.lower() in english_vocab]
+
+
+def word_tokenize(text):
+    """
+    convert a string to list of normal word tokens
+    """
+    return filter_unusual_words(filter_stopwords(tokenizer(text)))
+
+
+def get_word_count(x):
+    """
+    return normal word count, stop word count, unusual word count
+    """
+    wc = x.apply(lambda text: len(word_tokenize(text)))
+    stop_wc = x.apply(lambda text: len(get_stopwords(tokenizer(text))))
+    unusual_wc = x.apply(lambda text: len(get_unusual_words(tokenizer(text))))
+    return pd.DataFrame(
+        {
+            '{}_word_count'.format(x.name): wc,
+            '{}_stopword_count'.format(x.name): stop_wc,
+            '{}_unusual_word_count'.format(x.name): unusual_wc,
+            '{}_total_word_count'.format(x.name): wc + stop_wc + unusual_wc
+        }
+    )
